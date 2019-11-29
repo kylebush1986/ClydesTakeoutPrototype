e41e6b170a697fb6a87d39fb2bad9d4a5804d909
kylebush1986 <kylebush1986@gmail.com>
2019-11-29 10:22:30 -0700

    Employee interface styling and functionality. Added MenuDatabase class as a wrapper to pass database as object to Employee view.

---
+++ ClydesTakeoutPrototype/Models/OrderModels/OrderDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClydesTakeoutPrototype.Models.OrderModels
+{
+    public class OrderDatabase
+    {
+        public List<Order> AllOrders { get; set; }
+
+        public OrderDatabase(List<Order> orders)
+        {
+            AllOrders = orders;
+        }
+    }
+}
